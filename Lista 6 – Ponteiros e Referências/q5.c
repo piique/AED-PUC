26fbe6f8fecd815960d4ff7ca23b419bbf2611af
Pedro Vilaça <pedrovilacav@gmail.com>
2020-04-20 22:02:25 -0300

    Correção questão 5 lista 6

--- Lista 6 – Ponteiros e Referências/q5.c
+++ Lista 6 – Ponteiros e Referências/q5.c
@@ -11,10 +11,21 @@ void main()
 {
   char A = 'A';
   int B = 10;
-  float C = 10.0;
-  double D = 10.0;
+  float C = 12.0;
+  double D = 14.0;
+
+  char *Ap = &A;
+  int *Bp = &B;
+  float *Cp = &C;
+  double *Dp = &D;
+
   printf("\n\nO endereco do char A e %p . Essa variavel ocupa %d bytes.\n", &A, sizeof(A));
   printf("O endereco do int B e %p . Essa variavel ocupa %d bytes.\n", &B, sizeof(B));
   printf("O endereco do float C e %p . Essa variavel ocupa %d bytes.\n", &C, sizeof(C));
   printf("O endereco do double D e %p . Essa variavel ocupa %d bytes.\n\n", &D, sizeof(D));
+
+  printf("\nO endereco do char *A e %p . Essa variavel ocupa %d bytes.\n", &Ap, sizeof(Ap));
+  printf("O endereco do int *B e %p . Essa variavel ocupa %d bytes.\n", &Bp, sizeof(Bp));
+  printf("O endereco do float *C e %p . Essa variavel ocupa %d bytes.\n", &Cp, sizeof(Cp));
+  printf("O endereco do double *D e %p . Essa variavel ocupa %d bytes.\n\n", &Dp, sizeof(Dp));
 }

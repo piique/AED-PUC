0345bcf1d3de7eeeefdfa579b6b7203b4b89a4cd
piique <pedrovilacav@gmail.com>
2020-05-16 15:07:39 -0300

    Arquivos de configurações adicionados

--- Lista 9 - Recursividade e Strings/q1.c
+++ Lista 9 - Recursividade e Strings/q1.c
@@ -10,16 +10,20 @@ int main()
 
   if (resultado == -1)
   {
-    printf("Nao foi encontrado o valor %i no vetor", k);
+    printf("Nao foi encontrado o valor %i no vetor\n", k);
   }
   else
   {
-    printf("O valor %i foi encontrado no vetor na posicao %i. Vetor[%i]: %i", k, resultado, k, resultado);
+    printf("O valor %i foi encontrado no vetor na posicao %i. Vetor[%i]: %i\n", k, resultado, k, resultado);
   }
-  getch();
   return 0;
 }
 
+/*
+Descrição: retorna a posição em que k foi encontrado ou -1
+Entradas: um vetor, seu tamanho e um valor a ser procurado 
+Saída: retorna a posicao que k foi encontrada ou -1
+*/
 int procuraNoVetor(int vetor[], int k, int tamanhoVetor)
 {
   if (vetor[tamanhoVetor - 1] == k || tamanhoVetor == 0)

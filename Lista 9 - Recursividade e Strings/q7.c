68f5566447973e60144c00ea2e0a3985349dd341
piique <pedrovilacav@gmail.com>
2020-05-19 14:17:53 -0300

    Pequenas alterações

--- Lista 9 - Recursividade e Strings/q7.c
+++ Lista 9 - Recursividade e Strings/q7.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-void verificaIgualdade(char[], char[], int, int);
+void verificaIgualdade(char[], char[], int);
 
 int main() {
   char v1[100], v2[100];
   printf("Digite duas palavras a serem verificadas: ");
   scanf("%s %s", v1, v2);
   fflush(stdin);
+  verificaIgualdade(v1, v2, 100);
+}
 
-  // utilizacao da funcao strncmp para verificar igualdade
-  if (strncmp(v1, v2, 100) == 0) {
+void verificaIgualdade(char *v1, char *v2, int tamanho) {
+  if (strncmp(v1, v2, tamanho) == 0) {
     printf("Palavras iguais\n");
   } else {
     printf("Palavras diferentes\n");
   }
-  // verificaIgualdade(v1, v2, 100, 100);
 }

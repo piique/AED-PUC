b6bca0879139f15b76a4afb14dc3c7386d9c34d8
Pedro Vilaça <pedrovilacav@gmail.com>
2020-10-30 11:51:26 -0300

    Retirara verificaçao de parentenses da main e colocado em arquivo separado

--- AED 2/Trabalhos Teorico/Trabalho Teorico 10/pilha/main.c
+++ AED 2/Trabalhos Teorico/Trabalho Teorico 10/pilha/main.c
@@ -175,32 +175,3 @@ void RetiraImpares(Pilha* pilha) {
     value = pop(&pilha_aux);
   }
 }
-
-// procedimento para validar parenteses de uma expressão matemática
-void verificaParenteses() {
-  char string[100];
-  Pilha pilha;
-  create_pilha(&pilha);
-
-  printf("\n\nVALIDADOR PARENTESES\n");
-  printf("Digite uma expressao: ");
-  scanf("%s", string);
-
-  int len = strlen(string);
-
-  for (int i = 0; i < len; i++) {
-    if (string[i] == '(') {
-      push(&pilha, string[i]);
-    } else if (string[i] == ')') {
-      if (pop(&pilha) == -1) {
-        printf("\nExpressão Matemática Incorreta\n");
-        return;
-      }
-    }
-  }
-  if (is_empty(&pilha)) {
-    printf("\nExpressão Matemática Correta\n");
-  } else {
-    printf("\nExpressão Matemática Incorreta\n");
-  }
-}

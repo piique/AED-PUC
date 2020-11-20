18150bcba759b5660721fa20075f972a504ea7b7
Pedro Vilaça <pedrovilacav@gmail.com>
2020-11-20 18:30:04 -0300

    Upando provas realizadas

--- AED 2/provas/main.c
+++ AED 2/provas/main.c
@@ -1,39 +1,39 @@
-#include <stdio.h>
-char f_pop(Pilha *p) {
-  if (p->tam == 0) {
-    return -1;
-  }
+// #include <stdio.h>
+// char f_pop(Pilha *p) {
+//   if (p->tam == 0) {
+//     return -1;
+//   }
 
-  int dado = p->topo->dado;
+//   int dado = p->topo->dado;
 
-  Celula *tmp = p->topo;
-  p->topo = p->topo->prox;
-  free(tmp);
+//   Celula *tmp = p->topo;
+//   p->topo = p->topo->prox;
+//   free(tmp);
 
-  p->tam--;
-  return dado;
-}
+//   p->tam--;
+//   return dado;
+// }
 
-int extrairdapilha(Pilha *p, int x) {
-  char digito;
-  Pilha recebe;
-  int aux = -1;
-  create_pilha(&recebe);
+// int extrairdapilha(Pilha *p, int x) {
+//   char digito;
+//   Pilha recebe;
+//   int aux = -1;
+//   create_pilha(&recebe);
 
-  digito = f_pop(p);
-  while (digito != -1) {
-    if (digito == x) {
-      aux = x;
-      break;
-    } else {
-      push(&recebe, digito);
-    }
-  }
+//   digito = f_pop(p);
+//   while (digito != -1) {
+//     if (digito == x) {
+//       aux = x;
+//       break;
+//     } else {
+//       push(&recebe, digito);
+//     }
+//   }
 
-  digito = f_pop(&recebe);
-  while (digito != -1) {
-    push(p, digito);
-    digito = f_pop(&recebe);
-  }
-  return aux;
-}
+//   digito = f_pop(&recebe);
+//   while (digito != -1) {
+//     push(p, digito);
+//     digito = f_pop(&recebe);
+//   }
+//   return aux;
+// }
